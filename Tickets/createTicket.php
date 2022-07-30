8e88661dbcae2df829b38a45baf8df678144177d
Gaurav Sanger <gks_96@outlook.com>
2022-07-30 16:56:39 -0500

    create ticket front end

--- Tickets/createTicket.php
+++ Tickets/createTicket.php
@@ -5,7 +5,6 @@
 
 
 <html lang="en">
-
 <body>
 
     <nav class="topnav">
@@ -19,6 +18,44 @@
         Create Ticket
     </h1>
 
-</body>
+    <form style="text-align:center; font-size:medium">
+        <label style=" color:black ; font-size:18px " for="priority">Set a level of priority</label>     
+        
+        <! Drop-down box to set priority level of issue >
+        <select id="priority" name="priority">
+            <option style=" background-color:lightgoldenrodyellow" value="1">1 - Minor Inconvenience</option>
+            <option style=" background-color:gold" value="2">2 - Inconvenience</option>
+            <option style=" background-color:goldenrod" value="3">3 - Slowing down work flow</option>
+            <option style=" background-color:lightcoral" value="4">4 - Barely able to work</option>
+            <option style=" background-color:red" value="5">5 - Stopped working pending fix</option>
+        </select>
+        <br />
+        <br />
+        
+        <! input box for issues >
+        <textarea placeholder="Please describe your issue here."></textarea>
+        <br />
 
+        <! input area for email> 
+        <input style="width:auto" type = "email" placeholder = "Email@EmailClient.com"/>
+
+        <! upload file >
+        <text> &nbsp  &nbsp Upload a picture:</text>
+        <input type = "file" name = "upload" accept = "image/*" />
+
+        <! Sumbit button >
+        <button type = "submit">Submit</button>
+        <br />
+
+        <! Ticket # return value upon submit >
+        <input style="border-radius:10px; background-color:mediumaquamarine" id="ticketReturn" placeholder=" Ticket # Return" readonly="readonly"/>
+        <br />
+        <br />
+        <br />
+
+        <! Existing Ticket button >
+        <a href="/Tickets/existingTicket.php" style="color:darkgoldenrod">Click Here If You Have An Existing Ticket</a>
+
+    </form>
+</body>
 </html>
