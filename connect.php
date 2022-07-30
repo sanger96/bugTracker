6eacae9872a89f9e82ebebd40e22b0144d4ab735
Gaurav Sanger <gks_96@outlook.com>
2022-07-30 14:45:26 -0500

    moved php script to its own file

---
+++ connect.php
@@ -0,0 +1,49 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "usbw";
+$database = "bugtracker";
+
+//create connection
+$mysqli = new mysqli($host, $username, $password, $database);
+if ($mysqli->connect_errno) {
+    echo "failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_errno;
+}
+
+//host info
+echo $mysqli->host_info . "\n";
+
+//creating a variable $sql (is sql statment) to select from column email an password from table accounts
+$sql = "SELECT email, password FROM accounts";
+//creating a variable $result (is sql statment) is results from searching through $msqli created in 23
+$result = $mysqli->query($sql);
+
+
+//displays table
+if ($result->num_rows > 0) {
+    //output data of each row
+    while($row = $result->fetch_assoc()){
+        echo "<br>" . "email: " . $row["email"] . " , Password: " . $row["password"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$sql = "SELECT priority, description, picture, ticket FROM bugs";
+//creating a variable $result (is sql statment) is results from searching through $msqli created in 23
+$result = $mysqli->query($sql);
+
+
+//displays table
+if ($result->num_rows > 0) {
+    //output data of each row
+    while($row = $result->fetch_assoc()){
+        echo "<br>" . "priority: " . $row["priority"] . " , Issue Description: " . $row["description"] . " , Temp Pic: " . $row["picture"] . " , Ticket Number: " . $row["ticket"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$mysqli->close();
+
+?>
