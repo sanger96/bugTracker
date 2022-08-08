24b4fb9ce66ea6822ef0ca7b1300a255ce0c4f93
Gaurav Sanger <gks_96@outlook.com>
2022-08-08 12:27:58 -0500

    added a page for search results from existing tickets

--- Tickets/searchTicket.php
+++ Tickets/searchTicket.php
@@ -1,20 +1,50 @@
-<?php 
-    //currently displays all previous tickets, needs just last ticekt
-    //opens database
-    include "connect.php";
-    $search = $_GET["search"];
+<! DOCTYPE html >
+<meta charset="utf-8" />
+<meta name="viewport" content="width=devicewidth, initial sclae=1.0" />
+<link rel="stylesheet" href="/styles.css" />
 
-    $sql = "SELECT priority, issue, email FROM tickets WHERE issue LIKE '%$search%'";
-    $result = $mysqli->query($sql);
 
-    if ($result->num_rows > 0) {
-        echo "<table><tr><th>priority</th><th>issue</th><th>email</th></tr>";
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "<tr><td>".$row["priority"]."</td><td>".$row["issue"]."</td><td>".$row["email"]."</td></tr>";
-        }
-        echo "</table>";
-    }
-    //closes database
-    $mysqli->close();
-?>
+<html lang="en">
+
+    <body>
+        <header>
+            <nav class="topnav">
+                <a href="/index.php">Home</a>
+                <a href="/Tickets/createTicket.php">Create Ticket</a>
+                <a href="/Tickets/existingTicket.php">Existing Tickets</a>
+                <a href="/devAccounts/devLogin.php">Developer Login</a>
+            </nav>
+        </header>
+
+        <br />
+
+        <?php
+            //currently displays all previous tickets, needs just last ticekt
+            //opens database
+            include "connect.php";
+            $search = $_GET["search"];
+
+            //title
+            echo "<h1>Search Results for $search <br /> <br /> <br /> <br /> <br /> <br />";
+
+            $sql = "SELECT priority, issue, email FROM tickets WHERE issue LIKE '%$search%'";
+            $result = $mysqli->query($sql);
+
+            if ($result->num_rows > 0) {
+                echo "<table><tr><th>priority</th><th>issue</th><th>email</th></tr>";
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    echo "<tr><td>".$row["priority"]."</td><td>".$row["issue"]."</td><td>".$row["email"]."</td></tr>";
+                }
+                echo "</table>";
+            }
+            //closes database
+            $mysqli->close();
+        ?>
+
+    </body>
+
+</html>
+
+
+
